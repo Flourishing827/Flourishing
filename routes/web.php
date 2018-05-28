3dc9d54fd070bfa540dd1c1e26c35b8adb404115
Flourishing <huangxiangrong827@163.com>
2018-05-28 22:35:18 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/report', function () {
+    return 'Report page';
+});
+
+Route::get('/about', function () {
+    return 'About page';
+});
+
+Route::resource('shop','ShopController');
+
+Route::resource('pay','PayController');
+
+Route::resource('user','UseoController');
+
+
